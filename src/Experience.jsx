17e9c6b638d3298a3bd2a95d85f233c26b04f0a3
sandesh <sandeshbhatta218@gmail.com>
2025-05-18 09:12:25 +0545

    making touch-action none to avoid reload.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,9 @@
-import { Environment, useGLTF, OrbitControls, Float } from "@react-three/drei";
+import {
+  PresentationControls,
+  Environment,
+  useGLTF,
+  Float,
+} from "@react-three/drei";
 
 export default function Experience() {
   const computer = useGLTF(
@@ -9,10 +14,12 @@ export default function Experience() {
       <Environment preset="city" />
 
       <color args={["#695b5b"]} attach="background" />
-      <OrbitControls makeDefault />
-      <Float>
-        <primitive object={computer.scene} position-y={-1.2} />
-      </Float>
+
+      <PresentationControls global>
+        <Float rotationIntensity={0.4}>
+          <primitive object={computer.scene} position-y={-1.2} />
+        </Float>
+      </PresentationControls>
     </>
   );
 }
